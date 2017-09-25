b252b0c03cd7be55fc62ff75b40eefd1020023ac
My Name <andau@email.com>
2017-09-25 22:30:05 +0200

    ignore tests with Teststate tolerable

--- eclipsePlugin/abapCI/src/abapci/handlers/AbapUnitHandler.java
+++ eclipsePlugin/abapCI/src/abapci/handlers/AbapUnitHandler.java
@@ -1,27 +1,19 @@
-
 package abapci.handlers;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.preference.IPreferenceStore;
-
 import com.sap.adt.communication.exceptions.CommunicationException;
 import com.sap.adt.tools.abapsource.abapunit.AbapUnitTask;
-import com.sap.adt.tools.abapsource.abapunit.IAbapUnitAlert;
 import com.sap.adt.tools.abapsource.abapunit.IAbapUnitResult;
-import com.sap.adt.tools.abapsource.abapunit.IAbapUnitResultItem;
 import com.sap.adt.tools.abapsource.abapunit.IAbapUnitService;
 import com.sap.adt.tools.abapsource.abapunit.TestItem;
 import com.sap.adt.tools.abapsource.abapunit.TestRunException;
 import com.sap.adt.tools.abapsource.abapunit.services.AdtServicesPlugin;
 import com.sap.adt.tools.abapsource.abapunit.services.IAdtServicesFactory;
 import abapci.AbapCiPlugin;
-import abapci.connections.ISapConnection;
-import abapci.connections.SapConnection;
-import abapci.connections.SapDemoConnection;
 import abapci.domain.TestResultSummary;
-import abapci.domain.TestState;
 import abapci.preferences.PreferenceConstants;
 import abapci.result.TestResultSummaryFactory;
 
@@ -34,7 +26,7 @@ import abapci.result.TestResultSummaryFactory;
 public class AbapUnitHandler extends AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		
+
 		String packageName = event.getParameter("1");
 		IPreferenceStore prefs = AbapCiPlugin.getDefault().getPreferenceStore();
 		String destinationId = prefs.getString(PreferenceConstants.PREF_ABAP_UNIT_DEV_PROJECT);
@@ -45,44 +37,24 @@ public class AbapUnitHandler extends AbstractHandler {
 		String testobjectUrl = "/sap/bc/adt/vit/wb/object_type/devck/object_name/" + packageName;
 		TestItem itemObject = new TestItem(testobjectUrl, testobjectUrl);
 		task.addTestItem(itemObject);
-		
+
+		TestResultSummary testResultSummary;
+
 		try {
-
 			IAbapUnitResult abapUnitResult = abapUnitService.executeUnitTests(task, false, packageName);
-			int numAlerts = abapUnitResult.getAlerts().size();
-			for (IAbapUnitResultItem item : abapUnitResult.getItems()) {
-				numAlerts = numAlerts + item.getAlerts().size();
-				for (IAbapUnitResultItem childItem : item.getChildItems()) {
-					for (IAbapUnitAlert alert : childItem.getAlerts()) {
-						if (!alert.getTitle().contains("Invalid parameter ID")) {
-							numAlerts = numAlerts + 1;
-						}
-					}
-					for (IAbapUnitResultItem childItem2 : childItem.getChildItems()) {
-						for (IAbapUnitAlert alert2 : childItem2.getAlerts()) {
-							if (!alert2.getTitle().contains("Invalid parameter ID")) {
-								numAlerts = numAlerts + 1;
-							}
-						}
-					}
-				}
-			}
-
-			TestState testState = (numAlerts == 0) ? TestState.OK : TestState.NOK;
-			return new TestResultSummary(packageName, testState);
-
-		} 
-		catch (TestRunException e) {
+			return TestResultSummaryFactory.create(packageName, abapUnitResult);
+		} catch (TestRunException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			testResultSummary = TestResultSummaryFactory.createUndefined(packageName);
+		} catch (CommunicationException e) {
+			// TODO Eventually Output message that connection to SAP was not
+			// successful
+			// !consider also not ABAP mode - where no connection to an ABAPsystem exists
+			e.printStackTrace();
+			testResultSummary = TestResultSummaryFactory.createUndefined(packageName);
 		}
-		catch (CommunicationException e) {
-		   //TODO Eventually Output message that connection to SAP was not successful
-		   // !consider also not ABAP mode - wherre no connection to an ABAP system exists  
-		   return TestResultSummaryFactory.createUndefined(packageName); 	
-		}
-	
-		
-		return null;
+
+		return testResultSummary;
 	}
 }
--- eclipsePlugin/abapCI/src/abapci/result/TestResultSummaryFactory.java
+++ eclipsePlugin/abapCI/src/abapci/result/TestResultSummaryFactory.java
@@ -1,38 +1,47 @@
 package abapci.result;
 
-
+import java.util.ArrayList;
+import java.util.List;
+import com.sap.adt.tools.abapsource.abapunit.AbapUnitAlertSeverity;
+import com.sap.adt.tools.abapsource.abapunit.IAbapUnitAlert;
 import com.sap.adt.tools.abapsource.abapunit.IAbapUnitResult;
 import com.sap.adt.tools.abapsource.abapunit.IAbapUnitResultItem;
-
 import abapci.domain.TestResultSummary;
 import abapci.domain.TestState;
 
 public class TestResultSummaryFactory {
-
 	private static final String UNDEFINED_PACKAGE_NAME = null;
 
-	private TestResultSummaryFactory() {}
-	
-	public static TestResultSummary create(String packageName, IAbapUnitResult abapUnitResult) 
-	{
-	    int numCritialAlerts = abapUnitResult.getAlerts().size();
-	    
-	    for(IAbapUnitResultItem abapUnitResultItem : abapUnitResult.getItems())
-	    {
-	    	numCritialAlerts = numCritialAlerts + getNumCriticalAlerts(abapUnitResultItem);     	
-	    }
-	    TestState testState = numCritialAlerts == 0 ? TestState.OK : TestState.NOK; 
+	private TestResultSummaryFactory() {
+	}
+
+	public static TestResultSummary create(String packageName, IAbapUnitResult abapUnitResult) {
+		int numCritialAlerts = getCriticalAlerts(abapUnitResult.getAlerts()).size();
+
+		for (IAbapUnitResultItem abapUnitResultItem : abapUnitResult.getItems()) {
+			numCritialAlerts = numCritialAlerts + getNumCriticalAlerts(abapUnitResultItem);
+		}
+		TestState testState = numCritialAlerts == 0 ? TestState.OK : TestState.NOK;
 		return new TestResultSummary(packageName, testState);
 	}
-	
-	private static int getNumCriticalAlerts(IAbapUnitResultItem abapUnitResultItem) 
-	{
-		int numCritialAlerts = abapUnitResultItem.getAlerts().size();
-	    for(IAbapUnitResultItem abapUnitResultSubItem : abapUnitResultItem.getChildItems())
-	    {
-	    	numCritialAlerts = numCritialAlerts + getNumCriticalAlerts(abapUnitResultSubItem);     	
-	    }
-	    return numCritialAlerts; 
+
+	private static int getNumCriticalAlerts(IAbapUnitResultItem abapUnitResultItem) {
+		int numCritialAlerts = getCriticalAlerts(abapUnitResultItem.getAlerts()).size();
+		for (IAbapUnitResultItem abapUnitResultSubItem : abapUnitResultItem.getChildItems()) {
+			numCritialAlerts = numCritialAlerts + getNumCriticalAlerts(abapUnitResultSubItem);
+		}
+		return numCritialAlerts;
+	}
+
+	private static List<IAbapUnitAlert> getCriticalAlerts(List<IAbapUnitAlert> alerts) {
+		List<IAbapUnitAlert> criticalAlerts = new ArrayList<IAbapUnitAlert>();
+		for (IAbapUnitAlert alert : alerts) {
+			if (alert.getSeverity() != AbapUnitAlertSeverity.TOLERABLE
+					&& !alert.getTitle().contains("Invalid parameter ID")) {
+				criticalAlerts.add(alert);
+			}
+		}
+		return criticalAlerts;
 	}
 
 	public static TestResultSummary createUndefined(String packageName) {
@@ -40,7 +49,6 @@ public class TestResultSummaryFactory {
 	}
 
 	public static TestResultSummary createUndefined() {
-		return createUndefined(UNDEFINED_PACKAGE_NAME); 
+		return createUndefined(UNDEFINED_PACKAGE_NAME);
 	}
-
 }
