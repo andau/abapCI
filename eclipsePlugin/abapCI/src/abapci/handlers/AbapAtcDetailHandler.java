30f3c9bcdcb3eca2971f870a2166ab454f4bc6ff
test <test@mail.com>
2018-03-08 00:09:23 +0100

    fixed toggle ok and failed state

---
+++ eclipsePlugin/abapCI/src/abapci/handlers/AbapAtcDetailHandler.java
@@ -0,0 +1,34 @@
+package abapci.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+
+/**
+ * Our sample handler extends AbstractHandler, an IHandler base class.
+ * 
+ * @see org.eclipse.core.commands.IHandler
+ * @see org.eclipse.core.commands.AbstractHandler
+ */
+public class AbapAtcDetailHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		try {
+			PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
+					.showView("abapci.views.AbapCiMainView");
+		} catch (PartInitException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+	
+	public boolean isSuppressed() 
+	{
+		return false; 
+	}
+}
